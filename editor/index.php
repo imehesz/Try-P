533e8394155f8bef7bb781cae19f097d0b0ee752
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2012-02-15 08:15:32 -0500

    initial commit

---
+++ editor/index.php
@@ -0,0 +1,132 @@
+<?php
+
+$options = array(
+    // which string should represent a tab for indentation
+    'tabsize' => 4,
+);
+
+/**
+ * PHP Console
+ *
+ * A web-based php debug console
+ *
+ * Copyright (C) 2010, Jordi Boggiano
+ * http://seld.be/ - j.boggiano@seld.be
+ *
+ * Licensed under the new BSD License
+ * See the LICENSE file for details
+ *
+ * Source on Github http://github.com/Seldaek/php-console
+ */
+if (!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'), true)) {
+    header('HTTP/1.1 401 Access unauthorized');
+    die('ERR/401 Go Away');
+}
+
+define('PHP_CONSOLE_VERSION', '1.3.0-dev');
+require 'krumo/class.krumo.php';
+
+ini_set('log_errors', 0);
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+$debugOutput = '';
+
+if (isset($_POST['code'])) {
+    $code = $_POST['code'];
+
+    if (get_magic_quotes_gpc()) {
+        $code = stripslashes($code);
+    }
+
+    // if there's only one line wrap it into a krumo() call
+    if (preg_match('#^(?!var_dump|echo|print|< )([^\r\n]+?);?\s*$#is', $code, $m) && trim($m[1])) {
+        $code = 'krumo('.$m[1].');';
+    }
+
+    // replace '< foo' by krumo(foo)
+    $code = preg_replace('#^<\s+(.+?);?[\r\n]?$#m', 'krumo($1);', $code);
+
+    // replace newlines in the entire code block by the new specified one
+    // i.e. put #\r\n on the first line to emulate a file with windows line
+    // endings if you're on a unix box
+    if (preg_match('{#((?:\\\\[rn]){1,2})}', $code, $m)) {
+        $newLineBreak = str_replace(array('\\n', '\\r'), array("\n", "\r"), $m[1]);
+        $code = preg_replace('#(\r?\n|\r\n?)#', $newLineBreak, $code);
+    }
+
+    ob_start();
+    eval($code);
+    $debugOutput = ob_get_clean();
+
+    if (isset($_GET['js'])) {
+        header('Content-Type: text/plain');
+        echo $debugOutput;
+        die('#end-php-console-output#');
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Debug Console</title>
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+        <script src="jquery-1.7.1.min.js"></script>
+        <script src="ace/ace.js"></script>
+        <script src="ace/mode-php.js"></script>
+        <script src="php-console.js"></script>
+        <script>
+            $.console({
+                tabsize: <?php echo json_encode($options['tabsize']) ?>
+            });
+        </script>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <div class="input">
+                <textarea style="overflow:hidden;" class="editor" id="editor" name="code"><?php echo (isset($_POST['code']) ? htmlentities($_POST['code'], ENT_QUOTES, 'UTF-8') : null) ?></textarea>
+				<div class="output"><?php echo $debugOutput ?></div>
+				<div style="clear:both;"></div>
+                <div class="statusbar">
+                    <span style="display:none;" class="position">Line: 1, Column: 1</span>
+                    <span class="copy">
+                        Copy selection: <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="110" height="14" id="clippy">
+                            <param name="movie" value="clippy/clippy.swf"/>
+                            <param name="allowScriptAccess" value="always" />
+                            <param name="quality" value="high" />
+                            <param name="scale" value="noscale" />
+                            <param NAME="FlashVars" value="text=">
+                            <param name="bgcolor" value="#E8E8E8">
+                            <embed src="clippy/clippy.swf"
+                                   width="110"
+                                   height="14"
+                                   name="clippy"
+                                   quality="high"
+                                   allowScriptAccess="always"
+                                   type="application/x-shockwave-flash"
+                                   pluginspage="http://www.macromedia.com/go/getflashplayer"
+                                   FlashVars="text="
+                                   bgcolor="#E8E8E8"
+                            />
+                        </object>
+                    </span>
+                </div>
+            </div>
+            <input type="submit" name="subm" value="Run Code!" style="width:200px;"/>
+        </form>
+
+		<?php /*
+        <div class="help">
+            press ctrl-enter to submit
+            put '#\n' on the first line to enforce
+                \n line breaks (\r\n etc work too)
+        </div>
+        <div class="footer">
+            php-console v<?php echo PHP_CONSOLE_VERSION ?> - by <a href="http://seld.be/">Jordi Boggiano</a> - <a href="http://github.com/Seldaek/php-console">sources on github</a>
+        </div>
+		*/ ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,85 @@
-<?php echo "Hello world!"; ?>
+<?php
+
+error_reporting(E_ALL); 
+ini_set("display_errors", 1);
+
+$app = require __DIR__ . '/lib/base.php';
+
+$app->set('CACHE', false);
+$app->set('DEBUG',1);
+$app->set('UI','ui/');
+
+$app->set( 'html_title', 'Try-PHP.org');
+
+$app->route('GET /',function(){
+	F3::set( 'content', 'home.html' );
+	echo Template::serve( 'main.html' );
+});
+
+$app->route( 'GET /contact', function(){
+	F3::set( 'content', 'contact.html' );
+	echo Template::serve( 'main.html' );
+});
+
+$app->route( 'GET /learn', function(){
+	F3::set( 'content', 'learn.html' );
+	echo Template::serve( 'main.html' );
+});
+
+$app->route( 'GET /learn/lesson/@lesson_id', function(){
+	F3::set( 'lesson_id', '{{@PARAMS.lesson_id}}' );
+	$lesson_id = F3::get( 'lesson_id' );
+
+	// first let's load all the available lessons ...
+	$lessons = loadLessons();
+
+	if( ! in_array( $lesson_id, $lessons ) )
+	{
+		throw new Exception( 'Ooops, something bad happened ...' );
+	}
+
+	F3::set( 'lesson', 'lessons/lesson-' . $lesson_id . '.html' );
+
+	// let's check for next and previous lessons
+	F3::set( 'lesson_next', in_array( ($lesson_id+1), $lessons ) ? '/learn/lesson/' . ($lesson_id+1) : null );
+	F3::set( 'lesson_prev', in_array( ($lesson_id-1), $lessons ) ? '/learn/lesson/' . ($lesson_id-1) : null );
+
+	F3::set( 'content', 'learn.html' );
+	echo Template::serve( 'main.html' );
+});
+
+
+$app->route('GET /sayhi/@name', function(){
+	F3::set( 'html_title', 'Saying Hi - ' . F3::get( 'html_title' ) );
+	F3::set( 'name', '{{@PARAMS.name}}' );
+
+	F3::set( 'content', 'sayhi.html' );
+	echo Template::serve( 'main.html' );
+});
+
+// returning an array of available Lesson IDs
+// based on the files in the system
+// TODO implement some sort of caching so 
+//   we don't hit the hard drive so many times
+function loadLessons()
+{
+	$dir = 'ui/lessons/';
+	$lessons = glob( $dir . "lesson-*.html" );
+	$retval = array();
+
+	if( !empty( $lessons ) )
+	{
+		foreach( $lessons as $lesson )
+		{
+			$lesson_id = str_replace( $dir .'lesson-', '', str_replace( '.html', '', $lesson ) );
+			if( is_numeric( $lesson_id ) )
+			{
+				$retval[] = $lesson_id;
+			}
+		}
+	}
+
+	return $retval;
+}
+
+$app->run();
