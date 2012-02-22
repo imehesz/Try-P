35981021a2aedbe6b966e188c82bdb96a99d6c83
Imre Mehesz <imehesz@imehesz-BT456AAR-ABA-P6627C.(none)>
2012-02-22 15:41:02 -0500

    added title for lessons as a drop-down on the main lesson page

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $app->route( 'GET /learn/lesson/@lesson_id', function(){
 	// let's check for next and previous lessons
 	F3::set( 'lesson_next', in_array( ($lesson_id+1), $lessons ) ? '/learn/lesson/' . ($lesson_id+1) : null );
 	F3::set( 'lesson_prev', in_array( ($lesson_id-1), $lessons ) ? '/learn/lesson/' . ($lesson_id-1) : null );
+	F3::set( 'lessons', $lessons );
 
 	F3::set( 'content', 'learn.html' );
 	echo Template::serve( 'main.html' );
@@ -75,7 +76,50 @@ function loadLessons()
 		}
 	}
 
+	sort( $retval, SORT_NUMERIC );
 	return $retval;
+}
+
+/**
+ * getTitle 
+ * 
+ * given a lesson ID, returns a title
+ * // can be overwritten by $lesson_titles array() in config!
+ *
+ * @access public
+ * @return void
+ */
+function getTitle( $lesson_id )
+{
+	$title = '';
+	$lesson_titles = F3::get( 'lesson_titles' );
+
+	// we check if we have a pre-defined title for this class
+	if( isset( $lesson_titles ) && is_array( $lesson_titles ) )
+	{
+		if( array_key_exists( $lesson_id, $lesson_titles ) )
+		{
+			// we return the title and we're done
+			$title = $lesson_titles[$lesson_id];
+		}
+	}
+
+	if( ! $title )
+	{
+		$lesson_file = 'ui/lessons/lesson-' . $lesson_id . '.html';
+		if( file_exists( $lesson_file ) )
+		{
+			$file_to_string = file_get_contents( $lesson_file );
+			if( $file_to_string )
+			{
+				$end = strpos( $file_to_string, '</h3>', 1 );
+				$start = strpos( $file_to_string, '<h3>', 1 );
+				$title = substr( $file_to_string, $start + 4, $end - $start - 4 );
+			}
+		}
+	}
+
+	return $title;
 }
 
 $app->run();
